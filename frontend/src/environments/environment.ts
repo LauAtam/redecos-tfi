ca429e02b3e5d64843758b3c95d19bf399e6487b
LauAtam <lautaroatampiz@gmail.com>
2026-07-24 04:24:55 -0300

    fix(frontend): forzar trackeo de environments

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://tddzhhtzveqbatmqtjon.supabase.co',
+  supabaseKey: 'sb_publishable_e6OSCEI0ww3hm11TUiBuhg_Zxux-vE_',
+  apiUrl: 'https://redeco-api.onrender.com',
+  mercadoPagoPublicKey: 'APP_USR-f9252b9c-3093-4dfd-9944-e7a901d0a337',
+};
